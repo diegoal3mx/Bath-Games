1a889a56c849ce1b8b24347c0321be221d5cf2b1
Diego Juarez <diegoajm2003@gmail.com>
2023-03-21 23:54:47 -0600

    Initial Commit

---
+++ Bath Games/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  Bath Games
+//
+//  Created by Diego Juárez on 21/03/23.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ Bath Games/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Bath Games
+//
+//  Created by Diego Juárez on 21/03/23.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
